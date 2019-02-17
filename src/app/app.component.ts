acf9fcfa42b4cf4d66c2bf090942e540b034da3f
PLurka <pawellurka@gmail.com>
2019-02-17 14:21:05 +0100

    Utworzenie nowej aplikacji

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThoughtsService } from './thoughts.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'zlotemysli';
+  
+  constructor(
+    private thoughtsService: ThoughtsService
+  ) {}
+
+  private ngOnInit() {
+    this.thoughts = this.thoughtsService.fetchThoughts()
+  }
+
+  public text: String = "szczerozłota myśl"
+  public thoughts: Array<String> = []
+
+  public addThought(): void {
+    this.thoughts.push(this.text)
+    this.text = ""
+    this.thoughtsService.saveThoughts(this.thoughts)
+  }
+
+  public removeThought(i): void {
+    this.thoughts.splice(i, 1)
+    this.thoughtsService.saveThoughts(this.thoughts)
+  }
 }
