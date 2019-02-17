1ec1c79d964eca7c3deb84998c52021a9b75b662
PLurka <pawellurka@gmail.com>
2019-02-17 20:21:04 +0100

    Dodana obsługa pola autor, dodano pobieranie i wyświetlanie daty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ThoughtsService } from './thoughts.service';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
   selector: 'app-root',
@@ -13,20 +14,46 @@ export class AppComponent {
   ) {}
 
   private ngOnInit() {
-    this.thoughts = this.thoughtsService.fetchThoughts()
+    this.messages = this.thoughtsService.fetchMessages()
+    this.authors = this.thoughtsService.fetchAuthors()
+    this.dates = this.thoughtsService.fetchDates()
   }
 
-  public text: String = "szczerozłota myśl"
-  public thoughts: Array<String> = []
+  public message: String = "Twoja złota myśl..."
+  public author: String = "anonim"
+  public messages: Array<String> = []
+  public authors: Array<String> = []
+  public dates: Array<Date> = []
+  public date: Date = new Date();
+  public lastDate: Date = new Date();
 
   public addThought(): void {
-    this.thoughts.push(this.text)
-    this.text = ""
-    this.thoughtsService.saveThoughts(this.thoughts)
+    this.messages.push(this.message)
+    this.authors.push("~" + this.author)
+    this.dates.push(new Date())
+    this.thoughtsService.saveMessages(this.messages)
+    this.thoughtsService.saveAuthors(this.authors)
+    this.thoughtsService.saveDates(this.dates)
+    this.message = "Twoja złota myśl..."
+    this.author = "anonim"
   }
 
   public removeThought(i): void {
-    this.thoughts.splice(i, 1)
-    this.thoughtsService.saveThoughts(this.thoughts)
+    this.messages.splice(i, 1)
+    this.thoughtsService.saveMessages(this.messages)
+    this.authors.splice(i,1)
+    this.thoughtsService.saveAuthors(this.authors)
+    this.dates.splice(i,1)
+    this.thoughtsService.saveDates(this.dates)
+  }
+
+  public getDate(i: number): String{
+    this.date = this.dates[i];
+    this.lastDate = this.dates[i-1];
+    if(this.date){
+      if(!this.lastDate || (this.date.toDateString().match(this.lastDate.toDateString()) === undefined))
+        return this.date.toDateString()
+    }
+    return null
   }
 }
--- src/app/thoughts.service.ts
+++ src/app/thoughts.service.ts
@@ -7,12 +7,31 @@ export class ThoughtsService {
 
   constructor() { }
 
-  public saveThoughts(thoughts: Array<String>): void {
+  public saveAuthors(authors: Array<String>): void {
+    localStorage.setItem("authorsList", JSON.stringify(authors))
+  }
+
+  public saveMessages(thoughts: Array<String>): void {
     localStorage.setItem("thoughtsList", JSON.stringify(thoughts))
   }
 
-  public fetchThoughts(): Array<String> {
+  public saveDates(dates: Array<Date>): void {
+    localStorage.setItem("datesList", JSON.stringify(dates))
+  }
+
+  public fetchMessages(): Array<String> {
     let data = JSON.parse(localStorage.getItem("thoughtsList"))
     return data ? data : []
   }
+
+  public fetchAuthors(): Array<String> {
+    let data = JSON.parse(localStorage.getItem("authorsList"))
+    return data ? data : []
+  }
+
+  public fetchDates(): Array<Date> {
+    let data = JSON.parse(localStorage.getItem("datesList"))
+    return data ? data : []
+  }
+
 }
